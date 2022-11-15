7fc35bf5eafd725928e7c3f6e2280aef0df93e84
KhunkhunThanawich <khunkhun.thanawich@gmail.com>
2022-11-16 04:25:26 +0700

    fix: app crash when no input

--- app/src/main/java/com/example/fibonaccifunc/fragment/FragmentMain.kt
+++ app/src/main/java/com/example/fibonaccifunc/fragment/FragmentMain.kt
@@ -6,8 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.fibonaccifunc.function.Fibonacci
 import com.example.fibonaccifunc.databinding.FragmentMainBinding
+import com.example.fibonaccifunc.function.Fibonacci
 
 class FragmentMain : Fragment() {
     private var _binding: FragmentMainBinding? = null
@@ -18,7 +18,7 @@ class FragmentMain : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -26,23 +26,21 @@ class FragmentMain : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         init()
-
     }
 
 
     private fun init() {
-        binding.btnFindMember.setOnClickListener(View.OnClickListener {
-            userInput = Integer.parseInt(binding.ediTextUserInput.text.toString())
-//            Toast.makeText(context, "$userInput", Toast.LENGTH_SHORT).show()
-            if (userInput != null) {
-                val fibonacci = Fibonacci(userInput, context)
+        binding.btnFindMember.setOnClickListener {
+            if (binding.ediTextUserInput.text.isNullOrEmpty()) {
+                binding.txtShowAnswer.text = ""
+                Toast.makeText(context, "Please Input NUMBER before proceed", Toast.LENGTH_SHORT).show()
+            } else {
+                userInput = Integer.parseInt(binding.ediTextUserInput.text.toString())
+                val fibonacci = Fibonacci(userInput?:0)
                 fibonacci.findFibonacciMember()
                 binding.txtShowAnswer.text = fibonacci.getListOfMember().toString()
-            } else if (userInput == null) {
-                Toast.makeText(context, "Please Input NUMBER before proceed", Toast.LENGTH_SHORT)
-                    .show()
             }
-        })
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/fibonaccifunc/function/Fibonacci.kt
+++ app/src/main/java/com/example/fibonaccifunc/function/Fibonacci.kt
@@ -1,31 +1,17 @@
 package com.example.fibonaccifunc.function
 
-import android.content.Context
-import android.widget.Toast
-
-class Fibonacci(var userInput: Int?, private val context: Context?) {
-    private val memberOfFibonacci = arrayListOf<Int>(0, 1)
+class Fibonacci(var userInput: Int) {
+    private val memberOfFibonacci = if (userInput ==0) arrayListOf(0)
+    else arrayListOf(0,1)
 
     fun findFibonacciMember() {
-        var j: Int = 0
-         var sum: Int = 0
-//        Toast.makeText(context, "$userInput", Toast.LENGTH_SHORT).show()
-        for (i in 0..userInput!!) {
-            if (sum <= userInput!!) {
-                j = memberOfFibonacci[i + 1]
+       var sum = 0
+        for (i in 0..userInput){
+            if (sum <= userInput){
+                val j = memberOfFibonacci[i+1]
                 sum = memberOfFibonacci[i] + j
-                if (sum <= userInput!!) {
-                    memberOfFibonacci.add(sum)
-                }
-                Toast.makeText(
-                    context,
-                    "i = ${memberOfFibonacci[i]} /// j = $j /// sum = $sum ///  Input = $userInput ",
-                    Toast.LENGTH_SHORT
-                ).show()
             }
-        }
-        for (i in memberOfFibonacci) {
-            println(i)
+            if (sum<=userInput) memberOfFibonacci.add(sum)
         }
     }
 
